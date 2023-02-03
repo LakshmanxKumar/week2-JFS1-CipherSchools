473f36857c9e2d2228b120c3f0c5c8852c01e2fa
LakshmanxKumar <122230058+LakshmanxKumar@users.noreply.github.com>
2023-02-04 00:08:51 +0530

    Add files via upload

---
+++ Code4.java
@@ -0,0 +1,19 @@
+package week2;
+class A{
+    public void methodA(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public void methodB() {
+        System.out.println("B");
+    }
+}
+class C extends B{
+    public void methodC() {
+        System.out.println("C");
+    }
+}
+public class Code4 {
+
+}

425898d2855e111daf2426910b7cb8d0cd9984d7
LakshmanxKumar <122230058+LakshmanxKumar@users.noreply.github.com>
2023-02-04 00:04:46 +0530

    Add files via upload

---
+++ Code3.java
@@ -0,0 +1,18 @@
+package week2;
+class Father {
+    public final String lastName="Oberoi";
+    public void foodHabit() {
+        System.out.println("i prefer salty");
+    }
+}
+class Son extends Father {
+    public void foodHabit() {
+        System.out.println("I prefer sweet");
+    }
+}
+public class Code3 {
+	public static void main(String [] args) {
+		
+	}
+
+}

d2893a6b67bc040e01034fcc937b5c7d9dc3f184
LakshmanxKumar <122230058+LakshmanxKumar@users.noreply.github.com>
2023-02-04 00:20:00 +0530

    Add files via upload

---
+++ Code8.java
@@ -0,0 +1,16 @@
+package week2;
+import java.util.*;
+class Product{
+    String title;
+    int price;
+}
+public class Code8 {
+	  public static void main(String[] args) {
+	        ArrayList<Product> products=new ArrayList<Product>();
+	        Product p=new Product();
+	        p.title="Xmen";
+	        p.price=333;
+	        products.add(p);
+	    }
+
+}
